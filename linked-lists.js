7b52de334632f7a5755cf1b3888440e5e4be16d9
Cesar Li Chen <cesarli619@gmail.com>
2023-06-21 23:35:03 -0400

    Links js, and creates classes

---
+++ linked-lists.js
@@ -0,0 +1,13 @@
+class LinkedListNode {
+    constructor(value, next=null) {
+        this.value = value;
+        this.next = next;
+    }
+}
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+    }
+}
