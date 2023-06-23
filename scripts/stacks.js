11df9747f4028488bcf1e8d22d4d8fa7391baf0a
Cesar Li (WSL) <cesarli619@gmail.com>
2023-06-23 16:34:05 -0400

    Stacks completed, uses linked list as underlying ds

--- scripts/stacks.js
+++ scripts/stacks.js
@@ -2,4 +2,29 @@ class Stack {
     constructor() {
         this.ll = new LinkedList();
     }    
+
+    push(value) {
+        this.ll.prepend(value);
+    }
+
+    pop() {
+        return this.ll.removeFirst();
+    }
+
+    peek() {
+        return this.ll.first;
+    }
+
+    toString() {
+        return this.ll;
+    }
 }
+
+const myStack = new Stack();
+myStack.push(3);
+myStack.push(10);
+myStack.push(24);
+console.log(myStack.pop());
+console.log(myStack.peek());
+
+console.log(myStack.toString());
